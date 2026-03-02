a3659e13dbf4aa8457d2de8e7100efe06e352496
NathanFilipowitz <py58wpb@eduvaud.ch>
2026-03-02 16:22:08 +0100

    (feat) changed model to make use of settings.json for data persistence

--- src/models/model.py
+++ src/models/model.py
@@ -3,25 +3,86 @@ model.py
 
 Author:  Nathan Filipowitz
 Date:    2026-02-25
-Purpose: Handles Key-Value Database for the app, and methods to interact with it.
+Purpose: Handles json database for the app, and methods to interact with it for the controller.
 
 """
+import json
+import os
 
 class Model:
-    def __init__(self):
-        self.selected_path = None
-        self.is_protected = False
-        self.password = ""
-        self.logs_visible = True
+    def __init__(self, config_path="settings.json"):
+        self.config_path = config_path
+
+        # Temporary data
+        self.server_running = False
+        self.server_runner = None
+
+        # Saved data (initialization)
+        self.data = {
+            "selected_path": "",
+            "port": 8080,
+            "is_protected": False,
+            "password": "",
+            "logs_visible": True,
+            "qr_enabled": True
+        }
+        # Apply saved data
+        self.load_settings()
     
+    # Load settings from settings.json if it exists
+    def load_settings(self):
+        if os.path.exists(self.config_path):
+            with open(self.config_path, "r") as file:
+                self.data.update(json.load(file))
+                
+    # Write settings to json
+    def save_settings(self):
+        with open(self.config_path, "w") as f:
+            json.dump(self.data, f, indent=4)
+
+    # Get
+    def selected_path(self): 
+        return self.data["selected_path"]
+    
+    def port(self): 
+        return self.data["port"]
+
+    def qr_enabled(self): 
+        return self.data["qr_enabled"]
+
+    # Set
     def set_path(self, path):
-        self.selected_path = path
+        self.data["selected_path"] = path
+        self.save_settings()
     
-    def toggle_protection(self):
-        self.is_protected = not self.is_protected
+    def set_port(self, port):
+        self.data["port"] = int(port)
+        self.save_settings()
     
     def set_password(self, pwd):
-        self.password = pwd
+        if not pwd: # Delete password in database if the user removed it in the app
+            self.data["password"] = ""
+        else:
+            hashed_pwd = hashlib.sha256(pwd.encode()).hexdigest()
+            self.data["password"] = hashed_pwd
+        self.save_settings()
+    
+    def toggle_protection(self):
+        self.data["is_protected"] = not self.data["is_protected"]
+        self.save_settings()
+    
+    def toggle_qr(self):
+        self.data["qr_enabled"] = not self.data["qr_enabled"]
+        self.save_settings()
     
     def toggle_logs(self):
-        self.logs_visible = not self.logs_visible
+        self.data["logs_visible"] = not self.data["logs_visible"]
+        self.save_settings
+    
+
+    def check_password(self, input_pwd):
+        if not self.data["password"]:
+            return True
+            
+        input_hash = hashlib.sha256(input_pwd.encode()).hexdigest()
+        return input_hash == self.data["password"]
