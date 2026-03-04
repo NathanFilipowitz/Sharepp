df8008770864d46da9f4bab48b89fbbe7264f823
NathanFilipowitz <py58wpb@eduvaud.ch>
2026-03-04 15:49:53 +0100

    (feat)  replaced start and stop server buttons for icons to include them in the AppBar. Changed logs color scheme, changed update_result() function name to update_ui_for_path() and adapted logic to match maquette_app

--- src/views/view.py
+++ src/views/view.py
@@ -17,6 +17,8 @@ class View:
         self.controller = controller
         
         self.file_picker = ft.FilePicker()
+
+        self.pick_button = ft.ElevatedButton("Ouvrir un dossier", icon=ft.Icons.FOLDER_OPEN, on_click=self.controller.pick_folder)
         
         self.result_text = ft.Text(
             "Aucun dossier n'a encore été sélectionné",
@@ -24,10 +26,20 @@ class View:
             italic=True,
             overflow=ft.TextOverflow.CLIP
         )
-        
-        # Need to fix, not used but can't get them out either
-        self.btn_start = ft.Button("Démarrer le partage", on_click=self.controller.start_server, bgcolor=ft.Colors.GREEN, color=ft.Colors.WHITE)
-        self.btn_stop = ft.Button("Arrêter le partage", on_click=self.controller.stop_server, bgcolor=ft.Colors.RED, color=ft.Colors.WHITE, disabled=True)
+
+        # Start/Stop Server icon initially hidden
+        self.start_icon = ft.IconButton(
+            icon=ft.Icons.PLAY_ARROW_ROUNDED,
+            icon_color=ft.Colors.WHITE,
+            visible=False, 
+            on_click=self.controller.start_server,
+        )
+        self.stop_icon = ft.IconButton(
+            icon=ft.Icons.STOP_ROUNDED,
+            icon_color=ft.Colors.WHITE,
+            visible=False,
+            on_click=self.controller.stop_server,
+        )
         
         # Logs panel (right side)
         self.logs_column = ft.Column(
@@ -38,13 +50,13 @@ class View:
         
         self.logs_container = ft.Container(
             content=self.logs_column,
-            bgcolor=ft.Colors.WHITE,
+            bgcolor=ft.Colors.SURFACE_CONTAINER,
             border_radius=10,
             padding=10,
-            shadow=ft.BoxShadow(blur_radius=5, color=ft.Colors.BLACK12),
             visible=True,
             expand=True,
-            margin=20
+            margin=20,
+            ink=True
         )
         
         # Left side content
@@ -65,18 +77,8 @@ class View:
             center_title=True,
             bgcolor=ft.Colors.BLUE_200,
             actions=[
-                ft.IconButton(
-                    icon=ft.Icons.PLAY_ARROW_ROUNDED,
-                    icon_color=ft.Colors.WHITE,
-                    tooltip="Lancer le partage",
-                    on_click=self.controller.start_server,
-                ),
-                ft.IconButton(
-                    icon=ft.Icons.STOP_ROUNDED,
-                    icon_color=ft.Colors.WHITE,
-                    tooltip="Fermer le partage",
-                    on_click=self.controller.stop_server,
-                ),
+                self.start_icon,
+                self.stop_icon,
                 ft.IconButton(
                     icon=ft.Icons.CHAT_OUTLINED,
                     icon_color=ft.Colors.WHITE,
@@ -85,12 +87,12 @@ class View:
                 ),
                 ft.PopupMenuButton(
                     items=[
-                        ft.PopupMenuItem("About"),
-                        ft.PopupMenuItem(),  # divider
                         ft.PopupMenuItem(
-                            content="J'aime le CPNV",
-                            checked=False,
+                            content="Remember Last Path", 
+                            checked=self.controller.model.data["remember_path"],
+                            on_click=self.controller.toggle_remember_path
                         ),
+                        ft.PopupMenuItem("Clear Saved Path", on_click=self.controller.clear_path),
                     ],
                     icon_color=ft.Colors.WHITE
                 ),
@@ -111,13 +113,11 @@ class View:
                 on_click=self.toggle_password_visibility,
             )
 
+        # Main functionnalities Card (left column)
         self.toggle_card = ft.Card(
-            shadow_color=ft.Colors.ON_SURFACE_VARIANT,
             content=ft.Container(
                 content=ft.Column([
-                    ft.ElevatedButton("Pick Folder", icon=ft.Icons.FOLDER_OPEN, on_click=self.controller.pick_folder),
-                    # self.btn_start,
-                    # self.btn_stop,
+                    self.pick_button,
                     ft.Text("Répertoire:", weight=ft.FontWeight.BOLD),
                     ft.Container(
                         self.result_text,
@@ -152,14 +152,14 @@ class View:
         )
         
         self.page.add(self.main_row)
-        # self.page.bgcolor = ft.Colors.WHITE
     
+    # View function to add a log to the logs container
     def add_log_entry(self, message, level):
         color_map = {
-            "success": ft.Colors.GREEN,
-            "warning": ft.Colors.ORANGE,
-            "error": ft.Colors.RED,
-            "info": ft.Colors.BLACK,
+            "success": ft.Colors.LIGHT_GREEN_400,
+            "warning": ft.Colors.LIME_400,
+            "error": ft.Colors.DEEP_ORANGE_400,
+            "info": ft.Colors.INVERSE_PRIMARY,
         }
         log_color = color_map.get(level, ft.Colors.BLACK)
 
@@ -173,16 +173,29 @@ class View:
         self.logs_container.visible = not self.logs_container.visible
         self.page.update()
     
-    # rend le widget TextField visible et donne un aspect appuyé au chip lorsque l'entrée mdp est affichée
+    # turns the password TextField widget visible and gives a pressed look to the chip when enabled
     def toggle_password_visibility(self, e):
         self.password_entry.visible = not self.password_entry.visible
         e.control.selected = self.password_entry.visible
-    
-    def update_result(self, text):
-        self.result_text.value = text
+        self.controller.toggle_protection() 
         self.page.update()
 
+    # Updates title and shows/hides server icons if a path was selected
+    def update_ui_for_path(self, path, is_running=False):
+        if path:
+            self.appbar_title.value = f"Path: {path}"
+            self.result_text.value = path
+            self.start_icon.visible = not is_running
+            self.stop_icon.visible = is_running
+        else:
+            self.appbar_title.value = "SÉLECTIONNER UN DOSSIER"
+            self.start_icon.visible = False
+            self.stop_icon.visible = False
+        self.page.update()
+
+    # Toggle the icons between start and stop, and turns the left Card unavailable whenever the server is started.
     def update_server_status(self, running):
-        self.btn_start.disabled = running
-        self.btn_stop.disabled = not running
+        self.start_icon.visible = not running
+        self.stop_icon.visible = running
+        self.toggle_card.disabled = running
         self.page.update()
