d852cc4de02821c07b697ceb14eef1cc0557c576
NathanFilipowitz <py58wpb@eduvaud.ch>
2026-03-10 13:44:16 +0100

    refactor: changed the names of some variables and function, added comments and used unified logic for tmp_path import

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -13,36 +13,44 @@ import pytest
 from models.model import Model
 
 # Tests settings application on app opening, and settings file creation if missing.
-def test_model_initialization(tmp_path):
+def test_tu01_model_initialization(tmp_path):
+    # Arrange
     config_file = tmp_path / "test_settings.json"
     model = Model(config_path=str(config_file))
+
+    # Act
     model.save_settings()
     
+    # Assert
     assert model.data["port"] == 8080
     assert os.path.exists(config_file)
 
 # Tests password hashing from mock settings
-def test_password_hashing():
-    model = Model(config_path="test_temp.json")
+def test_tu02_password_hashing(tmp_path):
+    # Arrange
+    config_file = tmp_path / "test_temp_pwd.json"
+    model = Model(config_path="config_file")
     password = "reallyGoodPassword"
+
+    # Act
     model.set_password(password)
     
-    # pw not in clear, was hashed correctly, doesn't work with another pw
+    # Assert
+    # pw not in clear, was hashed correctly
     assert model.data["password"] != password
+    # wrong pw check
     assert model.check_password(password) is True
     assert model.check_password("falsePassword") is False
-    
-    # we remove the file created for the test
-    if os.path.exists("test_temp.json"):
-        os.remove("test_temp.json")
 
 # Tests qrcode generation and base64 encoding from url
-def test_qr_generation():
-    model = Model(config_path="test_temp.json")
-    qr_url = model.generate_qr_data_url("192.168.1.10")
+def test_tu03_qr_generation(tmp_path):
+    # Arrange
+    config_file = tmp_path / "test_temp_qr.json"
+    model = Model(config_path="config_file")
+
+    # Act
+    qr_base64 = model.generate_qr_data_url("192.168.1.10")
     
-    # just check if image was created
-    assert qr_url.startswith("data:image/png;base64,")
-    
-    if os.path.exists("test_temp.json"):
-        os.remove("test_temp.json")
+    # Assert
+    # check if image was created by it's header
+    assert qr_base64.startswith("data:image/png;base64,")
