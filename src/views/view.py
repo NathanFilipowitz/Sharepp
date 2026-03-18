060c651dbdb26c41cf5d79331dfd35334c0b5c3d
NathanFilipowitz <py58wpb@eduvaud.ch>
2026-03-18 16:24:37 +0100

    (feat) added AI generated icon to appbar instead of funny looking 'Share++'

--- src/views/view.py
+++ src/views/view.py
@@ -71,7 +71,7 @@ class View:
         )
         
         self.page.appbar = ft.AppBar(
-            leading=ft.Text("Share++", size=20, weight=ft.FontWeight.BOLD, color=ft.Colors.WHITE),
+            leading=ft.Image(src="icons/app_icon.svg", color=ft.Colors.BLUE_200),
             leading_width=100,
             title=self.appbar_title,
             center_title=True,
