b39afd89f1e7bbae8715ea316d1f78edb63831b9
NathanFilipowitz <py58wpb@eduvaud.ch>
2026-04-30 10:32:06 +0200

    (refactor[test]) changed variable name to adapt to Model parameter syntax

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -8,7 +8,6 @@ Purpose: Unit tests testing various app's Model methods
 """
 
 import os
-import json
 import pytest
 from models.model import Model
 
@@ -16,25 +15,28 @@ from models.model import Model
 def test_tu01_model_initialization(tmp_path):
     # Arrange
     config_file = tmp_path / "test_settings.json"
-    model = Model(config_path=str(config_file))
+    model = Model(config_name="test_settings.json")
+    model.config_dir = tmp_path
+    model.config_path = config_file
 
     # Act
     model.save_settings()
-    
+
     # Assert
     assert model.data["port"] == 8080
     assert os.path.exists(config_file)
 
-# Tests password hashing from mock settings
+
 def test_tu02_password_hashing(tmp_path):
     # Arrange
-    config_file = tmp_path / "test_temp_pwd.json"
-    model = Model(config_path=str(config_file))
+    model = Model(config_name="test_settings.json")
+    model.config_dir = tmp_path
+    model.config_path = tmp_path / "test_temp_pwd.json"
     password = "reallyGoodPassword"
 
     # Act
     model.set_password(password)
-    
+
     # Assert
     # pw not in clear, was hashed correctly
     assert model.data["password"] != password
